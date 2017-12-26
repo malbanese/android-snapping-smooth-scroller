46431a5e34990d912d5ae67455de92422876e3dd
Michael Albanese <6627835+malbanese@users.noreply.github.com>
2017-12-25 23:23:11 -0800

    Moved snapping options into a separate child class of SnappingSmoothScroller.

--- app/src/main/java/com/malba/sandbox/SnappingSmoothScroller.java
+++ app/src/main/java/com/malba/sandbox/SnappingSmoothScroller.java
@@ -25,81 +25,21 @@ public class SnappingSmoothScroller extends LinearSmoothScroller {
      */
     public static final int DIRECTION_Y = 1;
 
-    // The number of MS it will take to scroll one inch during seeking.
-    private float mMSPerInch = 25.0f;
-
-    // The location on the child which will snap to the parent/
-    private float[] mChildSnap = { 0.50f, 0.50f };
-
-    // The location on the parent which will snap to the child.
-    private float[] mParentSnap = { 0.25f, 0.75f };
-
-    // The interpolator that will be used during the target found phase
-    private Interpolator mTargetFoundInterpolator = new DecelerateInterpolator();
+    /**
+     * The options used to calculate snapping parameters.
+     */
+    private SnappingOptions mOptions;
 
     public SnappingSmoothScroller(Context context) {
         super(context);
     }
 
     /**
-     * @param msPerInch The number of MS it will take to animate over one inch of screen space,
-     *                  while seeking towards the view belonging to our scrolling position. Only
-     *                  utilized when the view is not yet bound to the RecyclerView.
+     * Sets the snapping options which are used to calculate snapping points.
+     * @param options The options used to calculate snapping points.
      */
-    public void setMSPerInch(float msPerInch) {
-        this.mMSPerInch = msPerInch;
-    }
-
-    /**
-     * Sets up the positions on the child which will attempt to snap to the corresponding position
-     * on the parent. If (value <= 1.0f), a percentage will be used. If (value > 1.0f) a pixel
-     * value will be used.
-     * @param firstLocation The first child snapping location.
-     * @param secondLocation The second child snapping location.
-     */
-    public void setChildSnappingLocations(float firstLocation, float secondLocation) {
-        mChildSnap[0] = firstLocation;
-        mChildSnap[1] = secondLocation;
-    }
-
-    /**
-     * Sets up the positions on the child which will attempt to snap to the corresponding position
-     * on the parent. If (value <= 1.0f), a percentage will be used. If (value > 1.0f) a pixel
-     * value will be used.
-     * @param location The child snapping location.
-     */
-    public void setChildSnappingLocation(float location) {
-        setChildSnappingLocations(location, location);
-    }
-
-    /**
-     * Sets up the positions on the parent which will be used as an anchor to snap to the
-     * corresponding position on the child. If (value <= 1.0f), a percentage will be used.
-     * If (value > 1.0f) a pixel value will be used.
-     * @param firstLocation The first parent snapping location.
-     * @param secondLocation The second parent snapping location.
-     */
-    public void setParentSnappingLocations(float firstLocation, float secondLocation) {
-        mParentSnap[0] = firstLocation;
-        mParentSnap[1] = secondLocation;
-    }
-
-    /**
-     * Sets up the positions on the parent which will be used as an anchor to snap to the
-     * corresponding position on the child. If (value <= 1.0f), a percentage will be used.
-     * If (value > 1.0f) a pixel value will be used.
-     * @param location The child snapping location.
-     */
-    public void setParentSnappingLocation(float location) {
-        setParentSnappingLocations(location, location);
-    }
-
-    /**
-     * @param targetFoundInterpolator Sets the interpolator which will be used when the scroll
-     *                                position's view is attached to the RecyclerView.
-     */
-    public void setTargetFoundInterpolator(Interpolator targetFoundInterpolator) {
-        this.mTargetFoundInterpolator = targetFoundInterpolator;
+    public void setSnappingOptions(SnappingOptions options) {
+        mOptions = options;
     }
 
     /**
@@ -135,15 +75,15 @@ public class SnappingSmoothScroller extends LinearSmoothScroller {
             if(direction == DIRECTION_X && layoutManager.canScrollHorizontally()) {
                 final int start = layoutManager.getDecoratedLeft(child) - params.leftMargin;
                 final int end = layoutManager.getDecoratedRight(child) + params.rightMargin;
-                snapResult[0] = calculateSnapLocation(start, end, mChildSnap[0]);
-                snapResult[1] = calculateSnapLocation(start, end, mChildSnap[1]);
+                snapResult[0] = calculateSnapLocation(start, end, mOptions.mChildSnap[0]);
+                snapResult[1] = calculateSnapLocation(start, end, mOptions.mChildSnap[1]);
             }
 
             if(direction == DIRECTION_Y && layoutManager.canScrollVertically()) {
                 final int start = layoutManager.getDecoratedTop(child) - params.topMargin;
                 final int end = layoutManager.getDecoratedBottom(child) + params.bottomMargin;
-                snapResult[0] = calculateSnapLocation(start, end, mChildSnap[0]);
-                snapResult[1] = calculateSnapLocation(start, end, mChildSnap[1]);
+                snapResult[0] = calculateSnapLocation(start, end, mOptions.mChildSnap[0]);
+                snapResult[1] = calculateSnapLocation(start, end, mOptions.mChildSnap[1]);
             }
         }
 
@@ -173,8 +113,8 @@ public class SnappingSmoothScroller extends LinearSmoothScroller {
                 end = layoutManager.getHeight() - layoutManager.getPaddingBottom();
             }
 
-            snapResult[0] = calculateSnapLocation(start, end, mParentSnap[0]);
-            snapResult[1] = calculateSnapLocation(start, end, mParentSnap[1]);
+            snapResult[0] = calculateSnapLocation(start, end, mOptions.mParentSnap[0]);
+            snapResult[1] = calculateSnapLocation(start, end, mOptions.mParentSnap[1]);
         }
 
         return snapResult;
@@ -225,7 +165,7 @@ public class SnappingSmoothScroller extends LinearSmoothScroller {
 
     @Override
     protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
-        return mMSPerInch / displayMetrics.densityDpi;
+        return mOptions.mMSPerInch / displayMetrics.densityDpi;
     }
 
     @Override
@@ -234,7 +174,85 @@ public class SnappingSmoothScroller extends LinearSmoothScroller {
         final int dy = calculateSnappingDistance(targetView, DIRECTION_Y);
 
         if(dx != 0 || dy != 0) {
-            action.update(-dx, -dy, 500, mTargetFoundInterpolator);
+            action.update(-dx, -dy, 500, mOptions.mTargetFoundInterpolator);
+        }
+    }
+
+    /**
+     * Class used to configure snapping options for a SnappingSmoothScroller instance.
+     */
+    public static class SnappingOptions {
+        // The number of MS it will take to scroll one inch during seeking.
+        private float mMSPerInch = 25.0f;
+
+        // The location on the child which will snap to the parent/
+        private float[] mChildSnap = { 0.50f, 0.50f };
+
+        // The location on the parent which will snap to the child.
+        private float[] mParentSnap = { 0.25f, 0.75f };
+
+        // The interpolator that will be used during the target found phase
+        private Interpolator mTargetFoundInterpolator = new DecelerateInterpolator();
+
+        /**
+         * @param msPerInch The number of MS it will take to animate over one inch of screen space,
+         *                  while seeking towards the view belonging to our scrolling position. Only
+         *                  utilized when the view is not yet bound to the RecyclerView.
+         */
+        public void setMSPerInch(float msPerInch) {
+            this.mMSPerInch = msPerInch;
+        }
+
+        /**
+         * Sets up the positions on the child which will attempt to snap to the corresponding position
+         * on the parent. If (value <= 1.0f), a percentage will be used. If (value > 1.0f) a pixel
+         * value will be used.
+         * @param firstLocation The first child snapping location.
+         * @param secondLocation The second child snapping location.
+         */
+        public void setChildSnappingLocations(float firstLocation, float secondLocation) {
+            mChildSnap[0] = firstLocation;
+            mChildSnap[1] = secondLocation;
+        }
+
+        /**
+         * Sets up the positions on the child which will attempt to snap to the corresponding position
+         * on the parent. If (value <= 1.0f), a percentage will be used. If (value > 1.0f) a pixel
+         * value will be used.
+         * @param location The child snapping location.
+         */
+        public void setChildSnappingLocation(float location) {
+            setChildSnappingLocations(location, location);
+        }
+
+        /**
+         * Sets up the positions on the parent which will be used as an anchor to snap to the
+         * corresponding position on the child. If (value <= 1.0f), a percentage will be used.
+         * If (value > 1.0f) a pixel value will be used.
+         * @param firstLocation The first parent snapping location.
+         * @param secondLocation The second parent snapping location.
+         */
+        public void setParentSnappingLocations(float firstLocation, float secondLocation) {
+            mParentSnap[0] = firstLocation;
+            mParentSnap[1] = secondLocation;
+        }
+
+        /**
+         * Sets up the positions on the parent which will be used as an anchor to snap to the
+         * corresponding position on the child. If (value <= 1.0f), a percentage will be used.
+         * If (value > 1.0f) a pixel value will be used.
+         * @param location The child snapping location.
+         */
+        public void setParentSnappingLocation(float location) {
+            setParentSnappingLocations(location, location);
+        }
+
+        /**
+         * @param targetFoundInterpolator Sets the interpolator which will be used when the scroll
+         *                                position's view is attached to the RecyclerView.
+         */
+        public void setTargetFoundInterpolator(Interpolator targetFoundInterpolator) {
+            this.mTargetFoundInterpolator = targetFoundInterpolator;
         }
     }
 }
